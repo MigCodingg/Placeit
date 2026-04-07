d7da50faf1377cbfa704601d740232d7a183a736
MigCodingg <gumoficial4@gmail.com>
2026-04-07 21:06:01 +0200

    numbers!

--- Assets/scripts/Box.cs
+++ Assets/scripts/Box.cs
@@ -1,16 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class Box : MonoBehaviour
 {
     [SerializeField] private int _maxPushes = 3;
-
+    [SerializeField] private TMP_Text m_TextComponent; // assign in inspector OR auto-find
 
     private int _pushCount = 0;
-    SpriteRenderer _spriteRenderer;
+    private SpriteRenderer _spriteRenderer;
+
     void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();    
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // 🔧 Auto-find text if not assigned
+        if (m_TextComponent == null)
+            m_TextComponent = GetComponentInChildren<TMP_Text>();
+
+        UpdateText();
     }
+
     public bool CanBePushed()
     {
         return _pushCount < _maxPushes;
@@ -19,17 +28,33 @@ public class Box : MonoBehaviour
     public void RegisterPush()
     {
         _pushCount++;
+        UpdateText();
     }
 
     public void SetMaxPushes(int value)
     {
-    _maxPushes = value;
-    _pushCount = 0;
-    
-    }
-    public void SetColor(Color color)
-    { 
-      _spriteRenderer.color = color;
+        _maxPushes = value;
+        _pushCount = 0;
+        UpdateText();
     }
 
+    public void SetColor(Color color)
+    {
+        _spriteRenderer.color = color;
+    }
+
+    void UpdateText()
+    {
+        if (m_TextComponent == null) return;
+
+        int remaining = _maxPushes - _pushCount;
+        m_TextComponent.text = remaining.ToString();
+
+        
+       
+        if (remaining <= 1)
+            m_TextComponent.color = Color.red;
+        else
+            m_TextComponent.color = Color.white;
+    }
 }
