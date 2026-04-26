71cf53f5cd2d9c4cb4f5feb16f116f8cfe9bc8cd
MigCodingg <gumoficial4@gmail.com>
2026-04-26 10:22:43 +0200

    sonido!

--- Assets/scripts/GridManager.cs
+++ Assets/scripts/GridManager.cs
@@ -1,43 +1,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class GridManager : MonoBehaviour
 {
-
-[System.Serializable]
+    [System.Serializable]
     public class BoxData
     {
-    public Vector2 position;
-    public int maxPushes;
-    public Color color;
+        public Vector2 position;
+        public int maxPushes;
+        public Color color;
     }
-    [SerializeField] private int _width, _height;
 
-    [SerializeField] private Tile _tilePrefab;
-    [SerializeField] private Color color;
-    [SerializeField] private GameObject _boxPrefab;
-    [SerializeField] private GameObject _playerPrefab;
-    [SerializeField] private List<Vector2> _trapPositions;
-    [SerializeField] private GameObject _trapPrefab;
-    [SerializeField] private Vector2 _playerStartPos = new Vector2(0, 0);
-
-    [SerializeField] private List<BoxData> _boxesData;
-
-    [SerializeField] private List<GoalData> _goalsData;
-    [SerializeField] private Color _goalColor = Color.green;
-
-    private Dictionary<Vector2, Tile> _tiles;
-    private Dictionary<Vector2, GameObject> _boxes;
-
-        [System.Serializable]
+    [System.Serializable]
     public class GoalData
     {
         public Vector2 position;
         public Color color;
     }
 
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private GameObject _boxPrefab;
+    [SerializeField] private GameObject _playerPrefab;
+
+    [SerializeField] private List<Vector2> _trapPositions;
+    [SerializeField] private GameObject _trapPrefab;
+
+    [SerializeField] private Vector2 _playerStartPos = new Vector2(0, 0);
+
+    [SerializeField] private List<BoxData> _boxesData;
+    [SerializeField] private List<GoalData> _goalsData;
+
+    // 🎵 Sounds
+    [SerializeField] private AudioClip _goalSound;
+    [SerializeField] private AudioClip _trapSound;
+
+    private AudioSource _audioSource;
+
+    private Dictionary<Vector2, Tile> _tiles;
+    private Dictionary<Vector2, GameObject> _boxes;
+
     void Start()
     {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+         _audioSource.volume = 0.7f;
+         
         GenerateGrid();
     }
 
@@ -68,14 +77,14 @@ public class GridManager : MonoBehaviour
             }
         }
 
-        
+        // 🎯 Color goal tiles
         foreach (var goal in _goalsData)
-    {
-        if (_tiles.TryGetValue(goal.position, out var tile))
         {
-            tile.SetColor(goal.color);
+            if (_tiles.TryGetValue(goal.position, out var tile))
+            {
+                tile.SetColor(goal.color);
+            }
         }
-    }
 
         // 📦 Spawn boxes
         foreach (var boxData in _boxesData)
@@ -97,7 +106,6 @@ public class GridManager : MonoBehaviour
 
         _boxes[pos] = boxObj;
     }
-
 
     public Tile GetTileAtPosition(Vector2 pos)
     {
@@ -121,7 +129,17 @@ public class GridManager : MonoBehaviour
         return _trapPositions.Contains(pos);
     }
 
-        public bool AreAllGoalsFilled()
+    public bool IsGoalPosition(Vector2 pos)
+    {
+        foreach (var goal in _goalsData)
+        {
+            if (goal.position == pos)
+                return true;
+        }
+        return false;
+    }
+
+    public bool AreAllGoalsFilled()
     {
         foreach (var goal in _goalsData)
         {
@@ -130,7 +148,8 @@ public class GridManager : MonoBehaviour
         }
         return true;
     }
-        public void MoveBox(Vector2 from, Vector2 to)
+
+    public void MoveBox(Vector2 from, Vector2 to)
     {
         if (_boxes.TryGetValue(from, out var box))
         {
@@ -139,21 +158,41 @@ public class GridManager : MonoBehaviour
             // 🕳 Trap check
             if (IsTrapPosition(to))
             {
+                // 💥 Play trap sound
+                if (_trapSound != null)
+                {
+                    _audioSource.pitch = Random.Range(0.9f, 1.1f);
+                    _audioSource.PlayOneShot(_trapSound);
+                    _audioSource.pitch = 1f;
+                }
+
                 Destroy(box);
             }
             else
             {
                 _boxes[to] = box;
+
+                // 🎯 Goal sound
+                if (IsGoalPosition(to))
+                {
+                    if (_goalSound != null)
+                    {
+                        _audioSource.pitch = Random.Range(0.95f, 1.05f);
+                        _audioSource.PlayOneShot(_goalSound);
+                        _audioSource.pitch = 1f;
+                    }
+                }
             }
 
-           
+            // 🏁 Win check
             if (AreAllGoalsFilled())
             {
                 LoadNextScene();
             }
         }
     }
-        void LoadNextScene()
+
+    void LoadNextScene()
     {
         int currentIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentIndex + 1);
